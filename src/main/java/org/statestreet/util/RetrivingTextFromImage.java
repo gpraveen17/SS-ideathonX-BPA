a228ce7500718a43c47bdade5fad70de58fce6c0
gpraveen17 <gpraveenk3@gmail.com>
2023-09-22 01:20:39 +0000

    updating code

--- src/main/java/org/statestreet/util/RetrivingTextFromImage.java
+++ src/main/java/org/statestreet/util/RetrivingTextFromImage.java
@@ -1,6 +1,9 @@
 package org.statestreet.util;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class RetrivingTextFromImage{
 
@@ -24,6 +27,52 @@ public static void main(String args[]) throws IOException {
     String imagePath = System. getProperty("user.dir")+"\\src\\resource\\Balance-Sheet.jpg";
     String textPath = System. getProperty("user.dir")+"\\src\\resource\\Balance-Sheet.txt";
     retrivingTextFromImage(imagePath,textPath);
+    HashMap<String, HashMap<String,String>> mapList = readDataFromTextFile(textPath);
+    System.out.println("map : "+mapList);
 
+}
+public static HashMap<String, HashMap<String,String>> readDataFromTextFile(String textFile){
+    HashMap<String,HashMap<String,String>> maplist = new HashMap<>();
+    HashMap<String,String> assetMap = new HashMap<>();
+    HashMap<String,String> liabilitiesMap = new HashMap<>();
+    try {
+        FileReader fr = new FileReader(textFile);
+        BufferedReader br = new BufferedReader(fr);
+        String str = "";
+        boolean assetFlag = true;
+        while((str=br.readLine())!=null){
+
+            if(str.contains("Liabilities")){
+                assetFlag=false;
+            }
+            if(assetFlag){
+                if(str.contains("$")){
+                    int index =0;
+                    char array[] = str.toCharArray();
+                    for(int i =0;i<array.length-1;i++){
+                        if(array[i]=='$'){
+                            index = i-1;
+                            break;
+                        }
+                    }
+                    //String strList[] = str.split("$");
+                    assetMap.put(str.substring(0,index),str.substring(index,str.length()));
+                }
+            } else{
+                if(str.contains("$")){
+                    if(str.contains("$")){
+                        String strList[] = str.split("$");
+                        liabilitiesMap.put(strList[0],strList[1]);
+                    }
+                }
+            }
+        }
+        maplist.put("Asset",assetMap);
+        maplist.put("Liabilities",liabilitiesMap);
+
+    }catch (Exception e ){
+        System.out.println(" Exception while reading Text file ");
+    }
+return maplist;
 }
 }
