50acf3baea78e1a6c061e36f62bb4d001eb9fa77
gpraveen17 <gpraveenk3@gmail.com>
2023-09-22 02:05:22 +0000

    api connection

--- src/main/java/org/statestreet/api/Services.java
+++ src/main/java/org/statestreet/api/Services.java
@@ -1,4 +1,106 @@
 package org.statestreet.api;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import java.io.BufferedReader;
+
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+
+import java.io.OutputStream;
+
+import java.io.OutputStreamWriter;
+
+import java.net.HttpURLConnection;
+
+import java.net.MalformedURLException;
+
+import java.net.ProtocolException;
+
+import java.net.URL;
+
 public class Services {
+
+    public static void POSTRequest() throws IOException {
+
+
+
+        final String POST_PARAMS = "{\n" + "\"userId\": 101,\r\n" +
+                "    \"id\": 105,\r\n" +
+                "    \"title\": \"Test Title\",\r\n" +
+                "    \"body\": \"Test Body\"" + "\n}";
+        System.out.println(POST_PARAMS);
+        URL obj = new URL("https://jsonplaceholder.typicode.com/posts");
+        HttpURLConnection postConnection = (HttpURLConnection) obj.openConnection();
+        postConnection.setRequestMethod("POST");
+        postConnection.setRequestProperty("userId", "a1bcdefgh");
+        postConnection.setRequestProperty("Content-Type", "application/json");
+
+
+        postConnection.setDoOutput(true);
+        OutputStream os = postConnection.getOutputStream();
+        os.write(POST_PARAMS.getBytes());
+        os.flush();
+        os.close();
+
+
+        int responseCode = postConnection.getResponseCode();
+        System.out.println("POST Response Code :  " + responseCode);
+        System.out.println("POST Response Message : " + postConnection.getResponseMessage());
+
+        if (responseCode == HttpURLConnection.HTTP_CREATED) { //success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    postConnection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in .readLine()) != null) {
+                response.append(inputLine);
+            } in .close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("POST NOT WORKED");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+//        POSTRequest();
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/api/greeting", (exchange -> {
+
+            if ("GET".equals(exchange.getRequestMethod())) {
+                String responseText = "Hello SS-ideathonX! For REST API\n";
+                exchange.sendResponseHeaders(200, responseText.getBytes().length);
+                OutputStream output = exchange.getResponseBody();
+                output.write(responseText.getBytes());
+                output.flush();
+            } else {
+                exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
+            }
+            exchange.close();
+        }));
+
+
+        server.setExecutor(null); // creates a default executor
+        server.start();
+
+    }
 }
--- src/main/java/org/statestreet/core/Helper.java
+++
@@ -1,4 +0,0 @@
-package org.statestreet.core;
-
-public class Helper {
-}
